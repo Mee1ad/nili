d5ca9b79d40ceb87aad026ffd09fd00e172127b1
Meelad <soheilravasani@gmail.com>
2022-08-07 23:27:30 +0300

    bug fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production'
+//         ? '/' + process.env.CI_PROJECT_NAME + '/'
+//         : '/'
+// }
+
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/' + process.env.CI_PROJECT_NAME + '/'
-        : '/'
+    publicPath: 'nili/'
 }
