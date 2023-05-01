3d7352edd10254ab08b7b15a6cbc819bc291fb6f
Meelad <soheilravasani@gmail.com>
2023-05-01 18:39:26 +0300

    change burger menu color

--- src/components/BurgerMenu.vue
+++ src/components/BurgerMenu.vue
@@ -2,7 +2,7 @@
   <div class="navbar-burger">
     <svg class="b-menu" width="50px" height="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g clip-path="url(#clip0_429_11066)">
-        <path d="M3 6.00092H21M3 12.0009H21M3 18.0009H21" stroke="#000" stroke-width="2.5" stroke-linecap="round"
+        <path d="M3 6.00092H21M3 12.0009H21M3 18.0009H21" stroke="#fff" stroke-width="2.5" stroke-linecap="round"
               stroke-linejoin="round"/>
       </g>
       <defs>
